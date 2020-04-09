a2a98a68933d5c402e46ac7b663012128f6f3065
alihassan87613 <alihassan87613@gmail.com>
2020-04-09 14:48:07 +0500

    changes for profile page information.

--- WcrAssociate/AngularAssociate/app/associate/components/profile/profile.component.ts
+++ WcrAssociate/AngularAssociate/app/associate/components/profile/profile.component.ts
@@ -41,6 +41,7 @@ export class ProfileComponent implements OnInit {
     isProfileFormVisible: boolean = false;
     profileImagePath:string = "";
     isProfileImageFormVisisble = false;
+    isHideInformation = true;
 
     validationMessages = {
         'password': {
@@ -152,9 +153,12 @@ export class ProfileComponent implements OnInit {
                                 thisStatus.profileForm.get('email').setValue($(docs).find("Email").text());
                                 thisStatus.profileForm.get('password').setValue($(docs).find("Password").text());
                                 thisStatus._messageService.filter('disable');
+                                thisStatus.isHideInformation = true;
                             }
                             else {
                                 thisStatus._messageService.filter('enable');
+                                thisStatus.isHideInformation = false;
+
                             }
                             thisStatus.isFormVisible = false;
 
